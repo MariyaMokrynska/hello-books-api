88fd0022ca4d48fb9dd7bb3e90e2764d1f46d511
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-17 00:08:46 -0700

    all books endpoint

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,3 +1,17 @@
 from flask import Blueprint
+from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.get("", strict_slashes=False) # treat a URI the same whether or not it ends in `/`
+def get_all_books():
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
