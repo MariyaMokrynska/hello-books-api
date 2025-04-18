267c8426c09f7956a8d4ae89d837f0bbf1f253b2
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-17 23:05:07 -0700

    handling invalid bookid: 400

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -3,8 +3,9 @@ from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-
 # treat a URI the same whether or not it ends in `/`
+
+
 @books_bp.get("", strict_slashes=False)
 def get_all_books():
     books_response = []
@@ -21,7 +22,11 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    book_id = int(book_id)
+    try:
+        book_id = int(book_id)
+    except:
+        return {"message": f"book {book_id} invalid"}, 400
+
     for book in books:
         if book.id == book_id:
             return {
