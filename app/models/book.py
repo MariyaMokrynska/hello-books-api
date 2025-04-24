e94ec474e16b97b3597fc263afe6e112c8915243
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-23 19:29:31 -0700

    book model set up

--- app/models/book.py
+++ app/models/book.py
@@ -10,3 +10,12 @@
 #     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
 #     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
 # ]
+
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
