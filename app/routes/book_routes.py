76f6c64a9faffe0c285dffedc989d801d6085df0
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-04-26 22:37:50 -0700

    update book model

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, Response, abort, make_response, request
 from app.models.book import Book
 from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 
 
 def validate_book(book_id):
